d235791cfd2ce90c04343a93a4cb96eed83707d4
Dan Q <dan@scatmania.org>
2015-07-21 17:01:54 +0100

    Two-way communication.

--- public/boats.js
+++ public/boats.js
@@ -1,6 +1,16 @@
 var firebase = new Firebase('https://sail.firebaseio.com/');
 var uid = null;
 var me = null;
+var connectionEstablished = false;
+
+$(function(){
+  $('#boat .controllable').change(function(){
+    changes = {}
+    changes[$(this).attr('id')] = $(this).val();
+    me.update(changes);
+  });
+});
+
 firebase.authAnonymously(function(error, auth){
   if (error) {
     alert('Authentication failed: ' + error);
@@ -8,7 +18,19 @@ firebase.authAnonymously(function(error, auth){
     uid = auth.uid;
     me = firebase.child('boats').child(uid);
     console.log('Authenticated as ', uid);
+    $('body').removeClass('finding-server').addClass('connecting');
     imStillHere();
+    me.on('value', function(data){
+      boat = data.val();
+      if(!connectionEstablished && boat.x){
+        $('body').removeClass('connecting').addClass('connected');
+        connectionEstablished = true;
+      }
+      $('#boat #x').val(boat.x);
+      $('#boat #y').val(boat.y);
+      $('#boat #angle').val(boat.angle);
+      $('#boat #speed').val(boat.speed);
+    });
   }
 });
 
--- server.js
+++ server.js
@@ -1,24 +1,53 @@
 var Firebase = require('firebase');
 var firebase = new Firebase('https://sail.firebaseio.com/');
 var lastTick = new Date();
+var localBoats = null;
 
-firebase.child('boats').on('child_added', function(child) {
-  console.log(child.key() + ' added');
-});
-firebase.child('boats').on('child_removed', function(child) {
-  console.log(child.key() + ' removed');
-});
-firebase.child('boats').on('child_changed', function(child) {
-  console.log(child.key() + ' changed:');
-  console.log(child.val());
+var playWidth = 1920;
+var playHeight = 1080;
+
+// clear all existing boats to begin with
+firebase.child('boats').remove();
+
+//firebase.child('boats').on('child_added', function(child) {
+//  console.log(child.key() + ' added');
+//});
+//firebase.child('boats').on('child_removed', function(child) {
+//  console.log(child.key() + ' removed');
+//});
+firebase.child('boats').on('value', function(boats) {
+  localBoats = boats.val();
+  console.log(boats.val());
 });
 
 function tick(){
-  // advance the clock
-  var timeSinceLastTick = new Date() - lastTick;
-  lastTick = new Date();
-  // move the boats
-  
-  setTimeout(tick, 1);
+  //console.log('tick: ');
+  if(!localBoats) {
+    //console.log('  no localBoats');
+  } else {
+    // advance the clock
+    var timeSinceLastTick = new Date() - lastTick;
+    lastTick = new Date();
+    // check for any newly-spawned boats that need starting data
+    Object.keys(localBoats).forEach(function(localBoatKey){
+      localBoat = localBoats[localBoatKey];
+      if(!localBoat.x){
+        console.log('spawning ', localBoatKey);
+        // spawning boat
+        firebase.child('boats/'+localBoatKey).update({
+          x: playWidth / 2,
+          y: playHeight / 2,
+          angle: Math.random() * 360,
+          speed: 0,
+          targetAngle: 0,
+          targetSpeed: 0,
+          anchor: 0,
+          targetAnchor: 0
+        });
+      }
+    });
+    // move the boats
+  }
+  setTimeout(tick, 1000);
 }
 tick();
